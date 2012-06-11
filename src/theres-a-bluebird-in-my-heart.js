fda0c92aff35e4eb0a00f3bd3a15cd847a23337d
Jared Hirsch <ohai@6a68.net>
2012-06-10 17:05:48 -0700

    update bluebird, add a page with publicly visible jquery code from meebo days

--- src/theres-a-bluebird-in-my-heart.js
+++ src/theres-a-bluebird-in-my-heart.js
@@ -1,7 +1,10 @@
-//  _there's a bluebird in my heart that_
-//  _wants to get out_
-//  _-- Bukowski_
+//  messing around with combinators, functional programming, and javascript.
+/* there's a bluebird in my heart that
+   wants to get out
+                            -- Bukowski */
 
-//  combinators, functional programming, javascript, and me.
-
-//  
+//  refs: 
+//
+//  *   https://github.com/evhan/combinator-birds
+//  *   https://github.com/raganwald/JQuery-Combinators
+//  *   functional.js? lazutkin? nathansuniversity? hm.
