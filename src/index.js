d0721af700337eaf0ca5a8039f06ebfc68dc5fa0
Jared Hirsch <ohai@6a68.net>
2012-08-28 23:44:12 -0700

    Reorganize index page chronologically

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 /*
     online: ohai at 6a68.net ||
            @jaredhirsch on twitter ||
-           6a68 on github || 
+           https://github.com/6a68 || 
            http://6a68.net || 
            http://jaredhirsch.com
 
@@ -13,38 +13,17 @@
 // 
 // ## articles:
 // ### note, *everything* is a draft at the moment :-)
-//
-// curious what's new? check the [github commit logs](https://github.com/6a68/6a68.github.com/commits/master)
 
-// ### producty
+// [fontbomb: somebody else's code, annotated](fontbomb.html)
+// <br>2012-07-07 &middot; *Code*
 
-// * Twitter with attenuation
-"I sometimes wish I could turn down the frequency of certain feeds
-within my global twitter stream. All you'd need is one slider per
-person, to limit them to 1 or 5 or 10 per day (or just apply no limit,
-the default standard setting). There's no article here yet; I might
-just build this out and see if I like it."
+"Fontbomb is a clever little project with a surprisingly well-written codebase.
+I had nothing to do with it and don't know the author, but I really enjoyed looking
+through the code--surprisingly well-written, for what looks like a fun little
+one-off project. I'm publishing my notes here as an experiment in public obscurity."
 
-// * [RSVP, email simplified](rsvp.html) 
-"Rethinking email; this is a running draft of a product concept. When
-it feels coherent enough, Ill start building it out."
-
-// ### geekcore
-
-// * [Re-entrant Exceptions in JS](reentrant-exceptions.html)
-"If you explicitly control the call stack, then you can fire a missing
-method exception, create the method, then resume execution. I learned
-about this through Smalltalk TDD literature. I've always wanted it in
-JS. So, here's an exploration of what it takes to get there."
-
-// * [Front-end architecture tradeoffs](frontend-architecture-patterns.html)
-"There's a lot of talk about lightweight vs heavyweight approaches to
-writing front-end code, but evangelists don't talk nearly as much about
-which problems are best solved by their favorite tool or framework. This
-is a first pass at listing the factors to consider before choosing the
-tools to use in building a given project."
-
-// * [JavaScript dependency management: an epic interview question](interview-problems-dependency-manager.html)
+// [JavaScript dependency management: an epic interview question](interview-problems-dependency-manager.html)
+// <br>2012-06-23 &middot; *Geekcore*
 "Building a dependency manager in JS touches on a lot of interesting areas
 you might want to cover in a programming interview, and it's a practical enough
 problem that people without a comp sci background could still have fun and
@@ -54,22 +33,37 @@ which is why I'm sharing it. The only thing missing is that my formulation of
 the problem doesn't test for DOM knowledge, but it's easily a big enough task
 to fill an hour with good discussion."
 
-// * [There's a Bluebird in my Heart: combinators in JS](theres-a-bluebird-in-my-heart.html)
+// [Front-end architecture tradeoffs](frontend-architecture-patterns.html)
+// <br>2012-06-22 &middot; *Geekcore*
+"There's a lot of talk about lightweight vs heavyweight approaches to
+writing front-end code, but evangelists don't talk nearly as much about
+which problems are best solved by their favorite tool or framework. This
+is a first pass at listing the factors to consider before choosing the
+tools to use in building a given project."
+
+// [Re-entrant Exceptions in JS](reentrant-exceptions.html)
+// <br>Last updated 2012-06-13 &middot; *Geekcore*
+"If you explicitly control the call stack, then you can fire a missing
+method exception, create the method, then resume execution. I learned
+about this through Smalltalk TDD literature. I've always wanted it in
+JS. So, here's an exploration of what it takes to get there."
+
+// [some jQuery components from meebo days, pulled from the CDN and annotated](meebo-jquery.html)
+// <br>2012-06-11 &middot; *Code*
+"todo: find some YUI components and post those (much more interesting JS)"
+
+// [There's a Bluebird in my Heart: combinators in JS](theres-a-bluebird-in-my-heart.html)
+// <br>Last updated 2012-06-10 &middot; *Geekcore*
 "JS coverage of birdie characters from _To Mock a Mockingbird_."
 
-// * [Functional Programming, thoughts](functional-programming.html)
+// [RSVP, email simplified](rsvp.html) 
+// <br>Last updated 2012-06-08 &middot; *Producty*
+"Rethinking email; this is a running draft of a product concept. When
+it feels coherent enough, Ill start building it out."
 
-// * [Code Equals Data Equals Code: JS metaprogramming](code-equals-data-equals-code.html)
+// [Functional Programming, thoughts](functional-programming.html)
+// <br>Last updated 2012-05-31 &middot; *Geekcore*
+
+// [Code Equals Data Equals Code: JS metaprogramming](code-equals-data-equals-code.html)
+// <br>Last updated 2012-05-31 &middot; *Geekcore*
 "If you keep things simple, going from code to data and back is tractable."
-
-// ### code
-
-// * [some jQuery components from meebo days, pulled from the CDN and annotated](meebo-jquery.html)
-// * todo: find some YUI components and post those (much more interesting JS)
-
-// * [fontbomb: somebody else's code, annotated](fontbomb.html)
-
-"Fontbomb is a clever little project with a surprisingly well-written codebase.
-I had nothing to do with it and don't know the author, but I really enjoyed looking
-through the code--surprisingly well-written, for what looks like a fun little
-one-off project. I'm publishing my notes here as an experiment in public obscurity."
